9a15ba20e05ed0ad6204019180ffc2747fef7f9d
devmoham <mohamed.mostafa432100@gmail.com>
2023-09-09 18:33:26 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Print numbers  0-9
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
