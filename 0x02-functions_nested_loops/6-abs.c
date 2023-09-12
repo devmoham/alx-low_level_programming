e4662c8dca7ac267a55925739da3920662bca337
devmoham <mohamed.mostafa432100@gmail.com>
2023-09-12 23:53:11 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of an integer
+ * @a: function parameter
+ *
+ * Return: -a or a
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a)
+	else
+		return (a)
+}
