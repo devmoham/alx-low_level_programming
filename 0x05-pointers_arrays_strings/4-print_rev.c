7897ee7e1ccc7f6c07a7a4984883e163ea2787fa
devmoham <mohamed.mostafa432100@gmail.com>
2023-09-18 16:07:25 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: string
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	for (o = len ; o > 0 ; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+
+
